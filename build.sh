212caa56c2a913c1ad6413743c40722ff23b5a9a
NolannB14 <nolann.bougrainville@epitech.eu>
2022-04-08 11:54:55 +0200

    [BIG TEST]

--- build.sh
+++
@@ -1,9 +0,0 @@
-EXECUTABLES="antman/antman,giantman/giantman,lib/my/libmy.a"
-
-IFS=',' read -ra ADDR <<< $EXECUTABLES
-for i in "${ADDR[@]}"; do
-    echo "$i"
-    if [[ ! -x "./${i}" ]]; then
-        exit 1
-    fi
-done
